05c8bf6658983836618e42a07a589cde8ce886f0
l-paz91 <54904891+l-paz91@users.noreply.github.com>
2023-01-09 22:35:37 +0000

    Create Tetris.h

---
+++ Console Window Modified/Tetris.h
@@ -0,0 +1,54 @@
+// -----------------------------------------------------------------------------
+#ifndef Tetris_H
+#define Tetris_H
+// -----------------------------------------------------------------------------
+
+//--INCLUDES--//
+#include <vector>
+#include <string>
+
+#include "InputOptions.h"
+#include "ObjectUpdateInterface.h"
+#include "TetrisRenderWindow.h"
+
+using namespace std;
+
+// -----------------------------------------------------------------------------
+
+class Tetris
+	: public ObjectUpdateInterface<TetrisRenderWindow, int>
+{
+public:
+	Tetris();
+
+	virtual void render(TetrisRenderWindow& pWindow);
+	virtual void update(const int& pDeltaTime);
+
+	const int getPlayerScore() const { return mScore; }
+
+private:
+	void setupTetrisBoard();
+	int getTetrominoBlockIndexAfterRotation(int pX, int pY, int pR);
+	bool doesPieceFit(int pTetromino, int pRotation, int pPosX, int pPosY);
+
+	InputOptions mTetrisInputOptions;
+	vector<char> mTetrisBoard;
+	vector<wstring> mTetrominos;
+
+	int mNumTetrominosOnBoard;
+	int mScore;
+
+	int mCurrentTetromino;
+	int mCurrentRotation;
+	int mCurrentX;
+	int mCurrentY;
+
+	int mTetrominoFallSpeed;
+	bool mForceTetrominoDown;
+	bool mLockRotate;
+};
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+#endif // !Tetris_H
